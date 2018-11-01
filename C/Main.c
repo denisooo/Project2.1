e5f56ec0eb99b11633790cd5c3a0719b30446970
Dennis <dennisharms42@gmail.com>
2018-11-01 11:09:23 +0100

    Made functions
    Made functions

--- C/Main.c
+++ C/Main.c
@@ -5,8 +5,26 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
+#include <include/Functions.c>
+
+// Globale variabelen
+float MAXTEMP;
+float MAXLIGHT;
+float MAXROLLOUT;
+float MINROLLOUT;
+float CURRENTDISTANCE;
+float ROLL_TO;
+float ROLL_TO_MIN;
+float ROLL_TO_MAX;
+
+union Param{
+  char chrs[4];
+  float f;
+} PROTO_PARAM;
+
 int main (void)
 {
+  ports();
   while(1) {
     _delay_ms(500);
   }
